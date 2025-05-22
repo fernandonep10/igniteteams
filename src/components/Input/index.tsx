b020277c16c40e9c6ba897dd04c9c6a32448511e
Fernando Nepomuceno <fernando.nep.s@gmail.com>
2025-05-21 22:50:53 -0300

    Erro de tipagem arrumado

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,6 @@
 import * as S from "./styles";
 import { TextInputProps } from "react-native";
-import { useTheme } from "styled-components";
 
 export default function Input({ ...rest }: TextInputProps) {
-  const { COLORS } = useTheme();
-
-  return <S.Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
+  return <S.Container {...rest} />;
 }
